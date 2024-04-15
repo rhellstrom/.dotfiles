ee42fa44ca0f6b85e0f9b592f5d46f7ad345a178
Rasmus Hellström <hellstromrasmus@gmail.com>
2024-04-15 15:01:30 +0300

    Initial commit

---
+++ nvim/after/plugin/lsp.lua
@@ -0,0 +1,63 @@
+local lsp_zero = require('lsp-zero')
+local cmp = require('cmp')
+
+cmp.setup({
+  sources = {
+    {name = 'nvim_lsp'},
+  },
+  mapping = {
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<Up>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+    ['<Down>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+    ['<C-p>'] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item({behavior = 'insert'})
+      else
+        cmp.complete()
+      end
+    end),
+    ['<C-n>'] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_next_item({behavior = 'insert'})
+      else
+        cmp.complete()
+      end
+    end),
+  },
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+})
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+
+--lsp_zero.set_sign_icons({
+--  error = '✘',
+--  warn = '▲',
+--  hint = '⚑',
+--  info = '»'
+--})
+-- to learn how to use mason.nvim
+-- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {
+	  'rust_analyzer',
+	  'pyright',
+	  'ansiblels',
+	  'yamlls',
+
+  },
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup({})
+    end,
+  },
+})
---
+++ nvim/lua/fisherman/init.lua
@@ -0,0 +1,4 @@
+require("fisherman.remap")
+require("fisherman.set")
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
