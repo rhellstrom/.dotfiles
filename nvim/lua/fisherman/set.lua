773a353f58869cbf6fcc6af2f58204a77c7e706e
rhellstrom <97554405+rhellstrom@users.noreply.github.com>
2025-09-12 10:48:24 +0300

    Update set.lua

--- nvim/lua/fisherman/set.lua
+++ nvim/lua/fisherman/set.lua
@@ -10,11 +10,11 @@ vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
-vim.opt.hlsearch = false
+vim.opt.hlsearch = true
 vim.opt.incsearch = true
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "108"
