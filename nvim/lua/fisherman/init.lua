744449939efad373103d8eba90c2e493dc064556
rhellstrom <97554405+rhellstrom@users.noreply.github.com>
2025-09-12 10:52:59 +0300

    Update init.lua

--- nvim/lua/fisherman/init.lua
+++ nvim/lua/fisherman/init.lua
@@ -2,4 +2,8 @@ require("fisherman.remap")
 require("fisherman.set")
 require("fisherman.lazy")
 vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
+-- vim.cmd([[colorscheme gruvbox]])
+vim.cmd.colorscheme "catppuccin"
+-- vim.cmd[[colorscheme tokyonight]]
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
